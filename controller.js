21d49fd366f4966a7891dcdf5ce7aecdf6aa6057
yyamakawa41 <yyamakawa@gmail.com>
2015-11-11 17:09:47 -0500

    controller update

--- controller.js
+++ controller.js
@@ -7,15 +7,14 @@ var singleVideoUrl = 'https://www.googleapis.com/youtube/v3/videos?id='+videoId+
 var playlistUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=8&playlistId='+playlistId+'+&key='+apiKey;
 	
 	$http.get(singleVideoUrl).success(function(youtubeSingle){
+		$scope.video = youtubeSingle.items
 		console.log(youtubeSingle.items[0].snippet);
-		$scope.youtubeSingle.items
+
 	}); 
 
 	$http.get(playlistUrl).success(function(youtubePlaylist){
-		console.log(youtubePlaylist);
-		$scope.youtubePlaylist = [
-
-		]
+		$scope.videos = youtubePlaylist.items
+		console.log($scope.videos);
 	});
 
 });
