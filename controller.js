8d2d70512892f8846cced95ed1cbef337a681cc2
yyamakawa41 <yyamakawa@gmail.com>
2015-11-11 15:12:30 -0500

    update max vids

--- controller.js
+++ controller.js
@@ -4,11 +4,18 @@ var apiKey = "AIzaSyDdJAcUcndlXwf4gbXuQOSRHX5XLwE5sRE";
 var videoId = "hakrt4XFzbg";
 var playlistId ="PL1NbHSfosBuHphFYJOY_L-XzyzzFLI2Gf";
 var singleVideoUrl = 'https://www.googleapis.com/youtube/v3/videos?id='+videoId+'&part=snippet,statistics&key='+apiKey;
-var playlistUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='+playlistId+'+&key='+apiKey;
+var playlistUrl = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=8&playlistId='+playlistId+'+&key='+apiKey;
 	
 	$http.get(singleVideoUrl).success(function(youtubeSingle){
-		console.log(youtubeSingle);
-		// $scope.videos = 
+		console.log(youtubeSingle.items[0].snippet);
+		$scope.youtubeSingle.items
 	}); 
+
+	$http.get(playlistUrl).success(function(youtubePlaylist){
+		console.log(youtubePlaylist);
+		$scope.youtubePlaylist = [
+
+		]
+	});
 
 });
